697710d917e1a04d707ffaea6a5bf022456d272a
yyc <yuanyinchun3@163.com>
2021-03-17 17:08:32 +0800

    udpate

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,7 +71,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/docs/')}>
+              to={useBaseUrl('docs/docs/introduction')}>
               Get Started
             </Link>
           </div>
